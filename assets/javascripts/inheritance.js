053f46aa23ba358843ca645b0ba4e6e428d296d9
Lovinder Pnag <lspnag@gmail.com>
2015-04-30 18:28:57 -0700

    finished collection

--- assets/javascripts/inheritance.js
+++ assets/javascripts/inheritance.js
@@ -244,7 +244,12 @@ var Album = Backbone.Collection.extend({
 	model: Song
 });
 
+var song1 = new Song({ name: "How Bizarre", artist: "OMC" });
+var song2 = new Song({ name: "Sexual Healing", artist: "Marvin Gaye" });
+var song3 = new Song({ name: "Talk It Over in Bed", artist: "OMC" });
 
+var myAlbum = new Album([ song1, song2, song3]);
+console.log( myAlbum.models );
 
 
 
